486e6d19aeef596ce14fc9649a9a4639057e40f4
SardorAllaberganov <sardorallaberganov96@gmail.com>
2019-07-22 15:58:52 +0500

    header-done

--- data/js/index.js
+++ data/js/index.js
@@ -50,3 +50,38 @@ function consoleText(words, id, colors) {
     }
   }, 400);
 }
+
+
+
+// $('header').mousemove(function(event){
+//   var moveX = (($(window).width() / 2) - event.pageX) * 0.01;
+//   var moveY = (($(window).height() / 2) - event.pageY) * 0.01;
+//   $('#header-bg').css('margin-left', moveX + 'px');
+//   $('#header-bg').css('margin-top', -moveY + 'px');
+// });
+
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $(".link-click").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
